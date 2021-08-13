614702fc616b63a0f306e47d95d89e698cbf3930
jsdawn <6556067+jsdawn@user.noreply.gitee.com>
2021-08-13 06:31:26 +0000

    删除文件 docker-build.sh

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-# docker login --username=*** registry.cn-shenzhen.aliyuncs.com
-
-docker build -t registry.cn-shenzhen.aliyuncs.com/jsdawn/soul-plus:0.0.1 .
-docker push registry.cn-shenzhen.aliyuncs.com/jsdawn/soul-plus:0.0.1
