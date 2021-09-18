6535ea607f9e96e52788004c0cd9b0fa590a2b89
jsdawn <kingwyh1993@163.com>
2021-09-18 19:07:36 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import vue from '@vitejs/plugin-vue';
+import styleImport from 'vite-plugin-style-import';
+const path = require('path');
+const resolve = dir => path.join(__dirname, dir);
+
+// https://vitejs.dev/config/
+export default {
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'soul-plus',
+          ensureStyleFile: true,
+          // 自定义样式文件转换
+          resolveStyle: name => {
+            return `soul-plus/es/${name}.css`;
+          },
+          resolveComponent: name => {
+            return `soul-plus/es/${name}`;
+          }
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      assets: resolve('src/assets'),
+      style: resolve('src/style'),
+      packages: resolve('packages')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import 'packages/theme-chalk/src/common/var.scss';`
+      }
+    }
+  },
+  build: {
+    input: {
+      main: path.resolve(__dirname, 'index.html')
+    },
+    rollupOptions: {}
+  }
+};
