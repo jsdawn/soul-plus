070def2ba801c1734405f50e3c34ccf2f0573f96
jsdawn <kingwyh1993@163.com>
2021-12-05 17:35:37 +0800

    add storybook dist

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,29 @@
 import vue from '@vitejs/plugin-vue';
-import styleImport from 'vite-plugin-style-import';
 const path = require('path');
 const resolve = dir => path.join(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'soul-plus',
-          ensureStyleFile: true,
-          // 自定义样式文件转换
-          resolveStyle: name => {
-            return `soul-plus/es/${name}.css`;
-          },
-          resolveComponent: name => {
-            return `soul-plus/es/${name}`;
-          }
-        }
-      ]
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve('src'),
       assets: resolve('src/assets'),
       style: resolve('src/style'),
-      packages: resolve('packages')
-    }
+      packages: resolve('packages'),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import 'packages/theme-chalk/src/common/var.scss';`
-      }
-    }
+        additionalData: `@import 'packages/theme-chalk/src/common/var.scss';`,
+      },
+    },
   },
   build: {
     input: {
-      main: path.resolve(__dirname, 'index.html')
+      main: path.resolve(__dirname, 'index.html'),
     },
-    rollupOptions: {}
-  }
+    rollupOptions: {},
+  },
 };
